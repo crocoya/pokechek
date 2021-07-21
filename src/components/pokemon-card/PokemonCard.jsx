1f626edf45ee70719c5e9f9f2f2592c1980425af
Crocoya <yassine.tababi@outlook.com>
2021-07-21 18:52:55 +0200

    personnaliz card pokemon

--- src/components/pokemon-card/PokemonCard.jsx
+++ src/components/pokemon-card/PokemonCard.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
 
-export default function PokemonCard({ pokemon }) {
+export default function PokemonCard({ pokemon, i }) {
   return (
-    <div className='pokemon__card'>
+    <div className='pokemon__card' key={i}>
       <div className='card__img'>
-        <img src={pokemon.sprites.front_default} alt='' />
+        <img src={pokemon.sprites.other.dream_world.front_default} alt='' />
       </div>
-      <div className='card__name'>{pokemon.name}</div>
+      <div className='card__name'>
+        <h3>{pokemon.name}</h3>
+      </div>
       <div className='card__types'>
-        {pokemon.types.map((type) => {
-          return <div className='card__type'>{type.type.name}</div>;
+        {pokemon.types.map((type, i) => {
+          return (
+            <div className='card__type' key={i}>
+              {type.type.name}
+            </div>
+          );
         })}
       </div>
       <div className='card__info'>
@@ -17,13 +23,15 @@ export default function PokemonCard({ pokemon }) {
           <p className='title'>Weight</p>
           <p>{pokemon.weight}</p>
         </div>
-        <div className='card__data card__data--height'>
-          <p className='title'>Height</p>
-          <p>{pokemon.height}</p>
-        </div>
-        <div className='card__data card__data--ability'>
-          <p className='title'>Ability</p>
-          <p>{pokemon.abilities[0].ability.name}</p>
+        <div className='card__info__more'>
+          <div className='card__data card__data--height'>
+            <p className='title'>Height</p>
+            <p>{pokemon.height}</p>
+          </div>
+          <div className='card__data card__data--ability'>
+            <p className='title'>Ability</p>
+            <p>{pokemon.abilities[0].ability.name}</p>
+          </div>
         </div>
       </div>
     </div>
